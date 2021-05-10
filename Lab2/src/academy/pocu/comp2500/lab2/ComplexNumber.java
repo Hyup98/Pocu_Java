59133c39f59d5fcd685087b398f5e4a3c6c7242e
Hyup <kkddong98@gmail.com>
2021-05-11 01:56:18 +0900

    5th

--- Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
+++ Lab2/src/academy/pocu/comp2500/lab2/ComplexNumber.java
@@ -5,61 +5,76 @@ public class ComplexNumber {
     public double real;
     public double imaginary;
 
-    public ComplexNumber(){
+    public ComplexNumber()
+    {
         this.real = 0.0;
         this.imaginary = 0.0;
     }
-    public  ComplexNumber(double real) {
+    public  ComplexNumber(double real)
+    {
         this.real = real;
         this.imaginary = 0.0;
     }
-    public  ComplexNumber(double real, double imaginary) {
+    public  ComplexNumber(double real, double imaginary)
+    {
         this.real = real;
         this.imaginary = imaginary;
     }
 
-    public  boolean isReal() {
-        if (imaginary == 0.0) {
+    public boolean isReal()
+    {
+        if (imaginary == 0.0)
+        {
             return true;
         }
 
-        else {
+        else
+        {
             return false;
         }
     }
 
-    public boolean isImaginary() {
-        if (real == 0.0) {
+    public boolean isImaginary()
+    {
+        if (real == 0.0)
+        {
             return true;
         }
-        else {
+
+        else
+        {
             return false;
         }
     }
 
-    public ComplexNumber getConjugate() {
+    public ComplexNumber getConjugate()
+    {
         ComplexNumber num = new ComplexNumber(this.real, -this.imaginary);
         return num;
     }
 
-    public  ComplexNumber add(ComplexNumber rhs) {
+    public  ComplexNumber add(ComplexNumber rhs)
+    {
         ComplexNumber num = new ComplexNumber(this.real + rhs.real, this.imaginary + rhs.imaginary);
         return num;
     }
 
-    public  ComplexNumber subtract(ComplexNumber rhs) {
+    public  ComplexNumber subtract(ComplexNumber rhs)
+    {
         ComplexNumber num = new ComplexNumber(this.real - rhs.real, this.imaginary - rhs.imaginary);
         return num;
     }
 
-    public  ComplexNumber multiply(ComplexNumber rhs) {
+    public  ComplexNumber multiply(ComplexNumber rhs)
+    {
         double temReal = this.real * rhs.real - this.imaginary * rhs.imaginary;
         double temImag = this.imaginary*rhs.real + this.real* rhs.imaginary;
         ComplexNumber num = new ComplexNumber(temReal, temImag);
         return num;
     }
 
-    public  ComplexNumber divide(ComplexNumber rhs) {
+    public  ComplexNumber divide(ComplexNumber rhs)
+    {
         ComplexNumber tem1 = new ComplexNumber(this.real ,this.imaginary);
         ComplexNumber tem2 = new ComplexNumber(rhs.real ,-rhs.imaginary);
         ComplexNumber tem3 = tem1.multiply(tem2);
