3e0d9487def9cf65bc597a6e8bb2f8f1a1dd5504
Hyup98 <40859624+Hyup98@users.noreply.github.com>
2021-05-11 09:29:18 +0900

    Update Article.java

--- Assignment1/src/academy/pocu/comp2500/assignment1/Article.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/Article.java
@@ -1,6 +1,8 @@
 package src.academy.pocu.comp2500.assignment1;
 
 public class Article {
-    public String tag;
+    public String title
     public String content;
+    // 댓글로 사용할 string배열
+    
 }
