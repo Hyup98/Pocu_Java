21e226d94bedb4b6b8f88f66b117fcdf8f35e966
Hyup98 <40859624+Hyup98@users.noreply.github.com>
2021-05-12 16:50:07 +0900

    Update User.java

--- Assignment1/src/academy/pocu/comp2500/assignment1/User.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/User.java
@@ -1,5 +1,13 @@
 package src.academy.pocu.comp2500.assignment1;
 
 public class User {
+  String userId;
+  User(String userId){
+    this.userId = userId;
+    
+  }
+ Blog BlogCreator(String userId) {
+   
+ }
 
 }
