821ef7d9b6714869a79736deaffe5801504e8d72
Hyup <kkddong98@gmail.com>
2021-05-21 03:38:57 +0900

    no message

--- Assignment1/src/academy/pocu/comp2500/assignment1/App.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/App.java
@@ -25,7 +25,7 @@ public class App {
         registry.registerCommentUpvoter("Comment", "upVoteToComment");// 댓글을 추천하는 메서드를 등록한다.
         registry.registerCommentDownvoter("Comment", "downVoteToComment");// 댓글을 비추천하는 메서드를 등록한다.
         registry.registerCommentListGetter("Article", "showComment");// 블로그 글에 달린 댓글들을 가져오는 메서드를 등록한다.
-        registry.registerSubcommentListGetter("Blog", "showComment");// 댓글에 달린 하위 댓글들을 가져오는 메서드를 등록한다.
+        registry.registerSubcommentListGetter("Article", "showComment");// 댓글에 달린 하위 댓글들을 가져오는 메서드를 등록한다.
         registry.registerSubcommentUpvoter("Comment", "upVoteToComment");// 하위 댓글을 추천하는 메서드를 등록한다.
         registry.registerSubcommentDownvoter("Comment", "downVoteToComment");// 하위 댓글을 비추천하는 메서드를 등록한다.
 
