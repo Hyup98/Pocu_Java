a28f7121406c5745cc67d14801f249b35672e552
Hyup <kkddong98@gmail.com>
2021-05-21 00:53:58 +0900

    no message

--- Assignment1/src/academy/pocu/comp2500/assignment1/App.java
+++ Assignment1/src/academy/pocu/comp2500/assignment1/App.java
@@ -1,6 +1,7 @@
-package src.academy.pocu.comp2500.assignment1;
+package academy.pocu.comp2500.assignment1;
 
-import src.academy.pocu.comp2500.assignment1.registry.Registry;
+import academy.pocu.comp2500.assignment1.registry.Registry;
+
 
 public class App {
     public App(Registry registry) {
